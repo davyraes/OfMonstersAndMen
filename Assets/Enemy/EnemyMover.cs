4979b74c83e793274aaa4c13e7a95c5a823529ba
davy <davy.raes@student.pxl.be>
2022-08-04 23:15:48 +0200

    pathfinding

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMover : MonoBehaviour
 {
-    List<Waypoint> path = new List<Waypoint>();
+    List<Tile> path = new List<Tile>();
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     Enemy enemy;
@@ -30,7 +30,7 @@ public class EnemyMover : MonoBehaviour
             .AddRange(
                 GameObject
                     .FindGameObjectWithTag("Path")
-                    .GetComponentsInChildren<Waypoint>());
+                    .GetComponentsInChildren<Tile>());
     }
 
     void ReturnToStart()
@@ -46,7 +46,7 @@ public class EnemyMover : MonoBehaviour
     
     IEnumerator FollowPath()
     {
-        foreach (Waypoint waypoint in path)
+        foreach (Tile waypoint in path)
         {
             yield return MoveTo(waypoint);
         }
@@ -54,7 +54,7 @@ public class EnemyMover : MonoBehaviour
         FinishPath();
     }
 
-    IEnumerator MoveTo(Waypoint waypoint)
+    IEnumerator MoveTo(Tile waypoint)
     {
         Vector3 startPosition = transform.position;
         Vector3 endPosition = waypoint.transform.position;
--- Assets/Envirement/CoordinateLabeler.cs
+++ Assets/Envirement/CoordinateLabeler.cs
@@ -7,17 +7,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
-    
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1, 0.5f, 0);
+
     TextMeshPro label;
-    Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
-    
+    Vector2Int coordinates;
+    GridManager gridManager;
+
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
-        
-        waypoint = GetComponentInParent<Waypoint>();
+
         DisplayCoordinates();
     }
 
@@ -35,16 +37,35 @@ public class CoordinateLabeler : MonoBehaviour
     }
 
     void SetLabelColor()
-        => label.color = waypoint.IsPlaceable
-            ? defaultColor
-            : blockedColor;
+    {
+        if (gridManager == null)
+        {
+            return;
+        }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null)
+        {
+            return;
+        }
+
+        label.color = node.isWalkable
+                ? node.isPath
+                    ? pathColor
+                    : node.isExplored
+                        ? exploredColor
+                        : defaultColor
+                : blockedColor;
+        
+    }
 
     void DisplayCoordinates()
     {
         Vector3 parentPosition = transform.parent.position;
-        coordinates.x = Mathf.RoundToInt(parentPosition.x/UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(parentPosition.z/UnityEditor.EditorSnapSettings.move.z);
-        
+        coordinates.x = Mathf.RoundToInt(parentPosition.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(parentPosition.z / gridManager.UnityGridSize);
+
         label.text = $"{coordinates.x},{coordinates.y}";
     }
 
